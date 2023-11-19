7b04abd0e030b4209e881f0f5e30894547e02451
Sheldon DSouza <sheldon.s.d@gmail.com>
2023-11-18 19:43:05 -0500

    Add media support and nemo actions

--- scripts/sd-update-mirrors.sh
+++ scripts/sd-update-mirrors.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
-su
-    cp /etc/pacman.d/mirrorlist /home/sheldon/Desktop/pacman.mirrorlist.$(date +%Y%m%d).backup
-    curl -s "https://archlinux.org/mirrorlist/?country=CA&country=US&country=DE&country=GB&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 10 - > /etc/pacman.d/mirrorlist
-exit
+sudo cp /etc/pacman.d/mirrorlist /home/sheldon/Desktop/pacman.mirrorlist.$(date +%Y%m%d).backup
+sudo curl -s "https://archlinux.org/mirrorlist/?country=CA&country=US&country=DE&country=GB&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 10 - > /etc/pacman.d/mirrorlist
